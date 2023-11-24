43434c80d49a90b5e7c4548a6e266f4be99ae2de
zjowowen <zjowowen@outlook.com>
2023-11-24 21:50:41 +0800

    polish code

--- ding/policy/__init__.py
+++ ding/policy/__init__.py
@@ -1,6 +1,6 @@
 from .base_policy import Policy, CommandModePolicy, create_policy, get_policy_cls
 from .common_utils import single_env_forward_wrapper, single_env_forward_wrapper_ttorch, default_preprocess_learn
-from .dqn import DQNSTDIMPolicy, DQNPolicy, DQNFastPolicy
+from .dqn import DQNSTDIMPolicy, DQNPolicy
 from .mdqn import MDQNPolicy
 from .iqn import IQNPolicy
 from .fqf import FQFPolicy
